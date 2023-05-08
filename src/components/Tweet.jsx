783dfbc91d60e2569b6d0967ee1305f08cf0b7a6
Adam Turi <aturike@gmail.com>
2023-05-08 15:36:25 +0200

    done

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,34 +1,23 @@
-function Tweet() {
+import Actions from "./Actions";
+import Message from "./Message";
+import ProfileImage from "./ProfileImage";
+import Timestamp from "./Timestamp";
+import User from "./User";
+
+function Tweet({ tweetData }) {
+  console.log(tweetData);
   return (
     <div className="tweet">
-      <img
-        src="https://i.imgur.com/9yw1Fyw.jpg"
-        className="profile"
-        alt="profile"
-      />
+      <ProfileImage image={tweetData.user.image} />
 
       <div className="body">
         <div className="top">
-          <span className="user">
-            <span className="name">Ironhack</span>
-            <span className="handle">@ironhack</span>
-          </span>
-
-          <span className="timestamp">Nov 30, 2020</span>
+          <User name={tweetData.user.name} handle={tweetData.user.handle} />
+          <Timestamp timestamp={tweetData.timestamp} />
         </div>
 
-        <p className="message">
-          On December 7th, we will be hosting a #webinar that will introduce you
-          to #SQL! Are you ready? 🚀
-        </p>
-
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i className="far fa-comment"></i>
-          <i className="fas fa-retweet"></i>
-          <i className="far fa-heart"></i>
-          <i className="fas fa-share"></i>
-        </div>
+        <Message message={tweetData.message} />
+        <Actions />
       </div>
 
       <i className="fas fa-ellipsis-h"></i>
